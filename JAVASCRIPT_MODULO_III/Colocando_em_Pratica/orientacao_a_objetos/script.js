3b3895a9ac1384c4c20202450d665040f91d4c00
Fabio Navarro <fabinho_1984@hotmail.com>
2022-06-18 15:48:43 -0300

    Adicionando mais 3 desafios nesta pasta

---
+++ JAVASCRIPT_MODULO_III/Colocando_em_Pratica/orientacao_a_objetos/script.js
@@ -0,0 +1,73 @@
+class ContaBancaria {
+    constructor(agencia, numero, tipo, saldo){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = 0;
+    }
+
+     get saldo(){
+        return this._saldo;
+     }
+
+     set saldo(valor){
+        this._saldo = valor;
+     }
+
+      sacar(valor){
+
+        if (valor > this._saldo) {
+            return 'Sem saldo suficiente para realizar esta operação'
+        }
+        this._saldo = this._saldo - valor;
+
+        return this._saldo;
+      }
+
+      depositar(valor){
+        this._saldo = this._saldo + valor;
+
+        return this._saldo;
+      }
+
+}
+
+class ContaCorrente extends ContaBancaria{
+    constructor(agencia, numero, cartaoCredito){
+        super(agencia, numero, cartaoCredito);
+        this.tipo = 'corrente';
+        this.cartaoCredito = cartaoCredito;
+    }
+
+    get cartaoCredito(){
+        return this._cartaoCredito;
+     }
+
+     set cartaoCredito(valor){
+        this._cartaoCredito = valor;
+     }
+    }
+
+     class contaPoupanca extends ContaBancaria{
+        constructor(agencia, numero){
+            super(agencia, numero);
+            this.tipo = 'poupança';
+            
+        }
+     }
+     class ContaUniversitaria extends ContaBancaria{
+        constructor(agencia, numero){
+            super(agencia, numero);
+            this.tipo = 'ContaUniversitaria';
+            
+        }
+        sacar(valor){
+            if (valor > 500) {
+                return "Operação negada";
+            }
+
+            this._saldo = this._saldo - valor;
+        }
+     }
+        
+
